85acf1653b68e235e90ecacccfda8c5d5e6a0631
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-20 10:55:37 -1000

    loops work, but palNumber is not being reassigned

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -8,15 +8,35 @@
 module.exports = function(digits){
   var factor_0 = 0;
   var factor_1 = 0;
-  var palindromeNumber = 0;
+  var palNumber = 0;
+  let palReverse;
+  let palArray;
 
-  // do your work here
-
-
+    if (digits === 2){
+      for (let i = 99; i >=10; i--){
+        factor_0 = i;
+        for (let j = 99; j>=10; j--){
+          factor_1 = j;
+          palNumber = factor_0 * factor_1;
+          palArray = palNumber.toString().split("").join();
+          palReverse = palNumber.toString().split("").reverse().join();
+          if (palArray === palReverse){
+            palNumber = palArray;
+            console.log(palNumber);
+        }
+      }
+        }
+  }
 
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
+    palNumber : palNumber,
   };
 };
+
+let palCheck = module.exports(2);
+console.log(palCheck);
+console.log(palCheck.palNumber);
+
+
